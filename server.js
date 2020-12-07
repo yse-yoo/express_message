c7c3b3dfb561ac5d6be7d3b637bf3bb8c4fb4b42
yse-yoo <youhei_yoshikawa@yse-c.net>
2020-12-07 12:05:45 +0900

    change server.js

--- server.js
+++ server.js
@@ -3,6 +3,54 @@ const express = require('express');
 
 //サーバ作成
 const app = express();
+//JSON対応
+app.use(express.json());
+// URLエンコードされたデータを解析する
+app.use(express.urlencoded({ extended: true }));
+
+//クロスドメインの許可  XSS
+app.use((req, res, next) => {
+    console.log(`middleware: all. url: ${req.url}`);
+
+    //CROS設定: 全てのドメインに対して許可 
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    //次の処理
+    next();
+});
+
+//静的コンテンツ
+app.use(express.static(__dirname + '/public'));
+
+
+//message.html から Ajax で POST リクエストされる
+app.post('/', (req, res) => {
+    let result = {
+        message: req.body.message,
+        datetime: new Date(),
+    };
+    //レスポンス
+    res.send(result);
+});
+
+app.post('/purchase', (req, res) => {
+    const tax_rate = 0.1;
+    const price = req.body.price;
+    const amount = req.body.amount;
+
+    const tax = price * amount * tax_rate;
+    const excluding_price = price * amount;
+    const total_price = excluding_price + tax;
+
+    let result = {
+        excluding_price: excluding_price,
+        total_price: total_price,
+        tax: tax,
+    };
+    //レスポンス
+    res.send(result);
+});
 
 //ルーティング
 // Webルートにリクエストされたらレスポンス
@@ -10,12 +58,6 @@ app.get('/', (req, res) => {
     res.send('Hello Express!');
 });
 //app.get(URLパス, 処理);
-
-//無名関数の書き方
-// app.get('/', function(req, res) {
-    
-// });
-
 
 app.listen(3000);
 
